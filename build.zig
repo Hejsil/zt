ed6de3a66ff104cc59eeff75e36c55e466bb74ed
Jimmi Holst Christensen <jhc@liab.dk>
2018-12-18 10:50:58 +0100

    Added build.zig

---
+++ build.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+
+const debug = std.debug;
+const mem = std.mem;
+
+const Builder = std.build.Builder;
+const LibExeObjStep = std.build.LibExeObjStep;
+
+pub fn build(b: *Builder) !void {
+    const fontconfig_cflags = try b.exec([][]const u8{ "pkg-config", "--cflags", "fontconfig" });
+    const freetype2_cflags = try b.exec([][]const u8{ "pkg-config", "--cflags", "freetype2" });
+    const cflags = try concat(b.allocator, []const u8, [][]const []const u8{
+        [][]const u8{
+            "-std=gnu99",
+            "-DVERSION=\"0.8.1\"",
+            "-D_XOPEN_SOURCE=600",
+        },
+        try split(b.allocator, fontconfig_cflags, " \t\r\n"),
+        try split(b.allocator, freetype2_cflags, " \t\r\n"),
+    });
+
+    const exe = b.addCExecutable("zt");
+    exe.addCompileFlags(cflags);
+    exe.addIncludeDir("/usr/X11R6/include");
+    exe.addLibPath("/usr/X11R6/lib");
+
+    linkLibs(exe, [][]const u8{
+        "m",
+        "rt",
+        "X11",
+        "util",
+        "Xft",
+    });
+    try linkPackageConfigLibs(b, exe, "fontconfig");
+    try linkPackageConfigLibs(b, exe, "freetype2");
+
+    inline for ([][]const u8{
+        "st",
+        "x",
+    }) |obj_name| {
+        const obj = b.addCObject(obj_name, obj_name ++ ".c");
+        obj.addCompileFlags(cflags);
+        exe.addObject(obj);
+    }
+
+    b.default_step.dependOn(&exe.step);
+}
+
+fn linkLibs(exe: *LibExeObjStep, libs: []const []const u8) void {
+    for (libs) |lib|
+        exe.linkSystemLibrary(lib);
+}
+
+fn linkPackageConfigLibs(b: *Builder, exe: *LibExeObjStep, package: []const u8) !void {
+    const libs = try b.exec([][]const u8{ "pkg-config", "--libs", package });
+    var lib_iter = mem.split(libs, " \t\r\n");
+    while (lib_iter.next()) |lib| {
+        if (!mem.startsWith(u8, lib, "-l"))
+            return error.Unexpected;
+
+        exe.linkSystemLibrary(lib[2..]);
+    }
+}
+
+fn concat(allocator: *mem.Allocator, comptime T: type, items: []const []const T) ![]T {
+    var res = std.ArrayList(T).init(allocator);
+    defer res.deinit();
+
+    for (items) |sub_items| {
+        for (sub_items) |item|
+            try res.append(item);
+    }
+
+    return res.toOwnedSlice();
+}
+
+fn split(allocator: *mem.Allocator, buffer: []const u8, split_bytes: []const u8) ![][]const u8 {
+    var res = std.ArrayList([]const u8).init(allocator);
+    defer res.deinit();
+
+    var iter = mem.split(buffer, split_bytes);
+    while (iter.next()) |s|
+        try res.append(s);
+
+    return res.toOwnedSlice();
+}
